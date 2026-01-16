aeac65d45a980b93b8fe5fe6677340f0dfff60e9
Salvador <salvaadr0@gmail.com>
2026-01-16 13:45:55 +0100

    arreglado la combiacion de las ramas

---
+++ src/main/java/com/dawm/sonara/entities/Artista.java
@@ -0,0 +1,33 @@
+package com.dawm.sonara.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "artista")
+public class Artista {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long artista_id;
+
+    @Column(name = "nombre_artistico", nullable = false, unique = true, length = 50)
+    private String nombre_artistico;
+
+    @Column(name = "pais_origen", nullable = false, length = 100)
+    private String pais;
+
+    @Column(name = "descripcion", nullable = false, length = 400)
+    private String descripcion;
+
+
+    public Artista(String nombre_artistico, String pais, String descripcion) {
+        this.nombre_artistico = nombre_artistico;
+        this.pais = pais;
+        this.descripcion = descripcion;
+    }
+}
