db520be8cf42cdc7869fce2239f371d80efebb07
Salvador <salvaadr0@gmail.com>
2026-02-20 10:49:24 +0100

    Entities

--- src/main/java/com/dawm/sonara/repositories/ArtistasRepository.java
+++ src/main/java/com/dawm/sonara/repositories/ArtistasRepository.java
@@ -1,6 +1,11 @@
 package com.dawm.sonara.repositories;
 
+import com.dawm.sonara.dtos.artistas.ArtistasCreateDTO;
+import com.dawm.sonara.dtos.artistas.ArtistasDTO;
+import com.dawm.sonara.dtos.artistas.ArtistasDetailDTO;
+import com.dawm.sonara.dtos.artistas.ArtistasUpdateDTO;
 import com.dawm.sonara.entities.Artista;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -8,4 +13,12 @@ import java.util.Optional;
 public interface ArtistasRepository extends JpaRepository<Artista, Long> {
     @Override
     Optional<Artista> findById(Long id);
+
+    ArtistasDTO getForEdit(Long id);
+    ArtistasDTO create(ArtistasCreateDTO dto);
+    ArtistasDTO update(ArtistasUpdateDTO dto);
+    void delete(Long id);
+    ArtistasDetailDTO getDetail(Long id);
+
+
 }
--- src/main/java/com/dawm/sonara/servicies/ArtistaServiceImpl.java
+++ src/main/java/com/dawm/sonara/servicies/ArtistaServiceImpl.java
@@ -1,4 +1,71 @@
 package com.dawm.sonara.servicies;
 
-public class ArtistaServiceImpl {
+import com.dawm.sonara.dtos.artistas.ArtistasCreateDTO;
+import com.dawm.sonara.dtos.artistas.ArtistasDetailDTO;
+import com.dawm.sonara.dtos.artistas.ArtistasUpdateDTO;
+import com.dawm.sonara.entities.Artista;
+import com.dawm.sonara.exceptions.DuplicateResourceException;
+import com.dawm.sonara.exceptions.ResourceNotFoundException;
+import com.dawm.sonara.mappers.ArtistasMapper;
+import com.dawm.sonara.repositories.ArtistasRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@Transactional
+public class ArtistaServiceImpl implements ArtistaService{
+
+    @Autowired
+    private ArtistasRepository artistasRepository;
+
+
+
+    @Override
+    public void create(ArtistasCreateDTO dto) {
+        if (artistasRepository.existsById(dto.getArtista_id())){
+            throw new DuplicateResourceException("artista", "nombre", dto.getNombre_artistico());
+        }
+        Artista artista = ArtistasMapper.toEntity(dto);
+        artistasRepository.save(artista);
+
+    }
+
+    @Override
+    public void update(ArtistasUpdateDTO dto) {
+        if (artistasRepository.existsById(dto.getArtista_id())){
+            throw new DuplicateResourceException("artista", "nombre", dto.getNombre_artistico());
+        }
+
+        Artista artista = artistasRepository.findById(dto.getArtista_id())
+                .orElseThrow(()-> new ResourceNotFoundException("artista", "nombre", dto.getArtista_id()));
+
+        ArtistasMapper.copyToExistingEntity(dto, artista);
+        artistasRepository.save(artista);
+
+    }
+
+    @Override
+    public void delete(Long id) {
+       if (!artistasRepository.existsById(id)){
+           throw new ResourceNotFoundException("artista", "nombre", id);
+       }
+       artistasRepository.delete(id);
+    }
+
+    @Override
+    public ArtistasUpdateDTO getForEdit(Long id) {
+        Artista artista = artistasRepository.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException("artista", "id", id));
+        return ArtistasMapper.toUpdateDTO(artista);
+    }
+
+    @Override
+    public ArtistasDetailDTO getDetail(Long id) {
+        Artista artista = artistasRepository.findById(id)
+                .orElseThrow(()-> new ResourceNotFoundException("artista", "id", id));
+        return ArtistasMapper.toDetailDTO(artista);
+    }
 }
