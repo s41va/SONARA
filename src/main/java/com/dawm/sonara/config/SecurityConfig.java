2a5e1394667c7270979c4d5ea8cd69549ea05fa9
Salvador Diaz <sdiarom170@g.educaand.es>
2026-05-13 20:41:03 +0200

    SecurityConfig para el endpoint de peticiones

--- src/main/java/com/dawm/sonara/config/SecurityConfig.java
+++ src/main/java/com/dawm/sonara/config/SecurityConfig.java
@@ -96,8 +96,10 @@ public class SecurityConfig {
 
                         // Ejemplos por roles
                         .requestMatchers("/api/usuarios/**").hasRole("ADMIN")
+                        .requestMatchers("/api/solicitudes").hasRole("USER")
                         .requestMatchers("/api/profile/perfil").hasAnyRole("ADMIN", "MANAGER", "USER")
                         .requestMatchers("/api/concierto/**").hasRole("ADMIN")
+                        .requestMatchers("/api/solicitudes/admin/**").hasRole("ADMIN")
                         .requestMatchers("/api/concierto").authenticated()
                         //.requestMatchers("/api/localidad**").hasAnyRole("ADMIN", "MANAGER")
                         //.requestMatchers("/api/profile**").hasRole("USER")                    // Solo USER
